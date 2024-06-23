0427af2bb0016cc0e140d801b312ca4228d1ad2c
ishu4748 <ishudhariya716@gmail.com>
2024-06-23 15:39:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function test(){
+ document.getElementById('bb').innerText = 'iyiyiyi';
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className="App" id="bb">
+     
+      
           Learn React
-        </a>
-      </header>
+          <button className='gjggjgj' onClick={test}>submit</button>
+          <div >Original Text</div>
     </div>
   );
 }
